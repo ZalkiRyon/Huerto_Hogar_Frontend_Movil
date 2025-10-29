143cd131f3ce5ddd92efda805b888d0a6dd96de6
Paula Frias est <pau.frias@duocuc.cl>
2025-10-29 20:07:47 -0300

    add : se crea el componente inputfield que sire para formularios manejando el cambio, los errores de un campo de tipo texto, se implementa funcionalmente la pantalla de registro

--- app/src/main/java/com/example/huerto_hogar/screen/registroScreen.kt
+++ app/src/main/java/com/example/huerto_hogar/screen/registroScreen.kt
@@ -1,9 +1,197 @@
 package com.example.huerto_hogar.screen
 
-import androidx.compose.runtime.Composable
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.huerto_hogar.repository.RegisterUserViewModel
+import com.example.huerto_hogar.repository.RegistrationResult
+import com.example.huerto_hogar.ui.theme.components.InputField
 
 @Composable
-fun RegistroScreen (navController: NavController){
+fun RegistroScreen(navController: NavController, viewModel: RegisterUserViewModel) {
+
+    val formState by viewModel.uiState.collectAsState()
+    val scrollState = rememberScrollState()
+
+    var showSuccessDialog by remember { mutableStateOf(false) }
+    var finalResult by remember { mutableStateOf<RegistrationResult?>(null) }
+    val context = LocalContext.current
+
+    val resultEvent = formState.registrationResultEvent
+    LaunchedEffect(resultEvent) {
+
+        if (resultEvent != null) {
+
+            finalResult = resultEvent
+
+            when (resultEvent) {
+                RegistrationResult.SUCCESS -> {
+                    showSuccessDialog = true
+                }
+
+                RegistrationResult.EMAIL_ALREADY_EXISTS -> {
+                    Toast.makeText(
+                        context,
+                        "Error: El correo electrónico ya está registrado.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+
+                RegistrationResult.ERROR -> {
+                    Toast.makeText(
+                        context,
+                        "Error al registrar. Verifique sus datos o intente más tarde.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+
+            viewModel.clearRegistrationResultEvent()
+        }
+    }
+
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+            .verticalScroll(scrollState),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(0.9f)
+                .padding(vertical = 32.dp, horizontal = 16.dp)
+                .background(color = Color.White, shape = RoundedCornerShape(8.dp))
+                .padding(16.dp),
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+
+            Text(
+                text = "Crear cuenta",
+                style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.Bold),
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+
+            InputField(
+                value = formState.name,
+                onValueChange = { viewModel.onChangeName(it) },
+                label = "Nombre",
+                error = formState.errors.nameError,
+                modifier = Modifier
+            )
+
+            InputField(
+                value = formState.lastname,
+                onValueChange = { viewModel.onChangeLastname(it) },
+                label = "Apellido",
+                error = formState.errors.lastnameError,
+                modifier = Modifier
+            )
+
+            InputField(
+                value = formState.email,
+                onValueChange = { viewModel.onChangeEmail(it) },
+                label = "Correo electronico",
+                placeholder = "Solo correos con @duoc.cl, @profesor.duoc.cl y @gmail.com",
+                error = formState.errors.emailError,
+                modifier = Modifier
+            )
+
+            InputField(
+                value = formState.password,
+                onValueChange = { viewModel.onChangePassword(it) },
+                label = "Contrasena",
+                error = formState.errors.passwordError,
+                modifier = Modifier
+            )
+
+            InputField(
+                value = formState.confirmPassword,
+                onValueChange = { viewModel.onChangeConfirmPassword(it) },
+                label = "Confirmar contrasena",
+                error = formState.errors.confirmPasswordError,
+                modifier = Modifier
+            )
+
+            InputField(
+                value = formState.address,
+                onValueChange = { viewModel.onChangeAddress(it) },
+                label = "Direccion",
+                error = formState.errors.addressError,
+                modifier = Modifier
+            )
+            InputField(
+                value = formState.phone,
+                onValueChange = { viewModel.onChangePhone(it) },
+                label = "Telefono (opcional)",
+                placeholder = "Solo números, máximo 9 dígitos",
+                error = formState.errors.phoneError,
+                modifier = Modifier
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = { viewModel.onClickRegister() },
+                modifier = Modifier.fillMaxWidth()
+
+
+            ) {
+
+                Text(text = "REGISTRAR", color = Color.White, fontSize = 18.sp)
+
+
+            }
+
+            if (showSuccessDialog) {
+                AlertDialog(
+                    onDismissRequest = { },
+                    title = { Text(text = "¡Registro Exitoso!") },
+                    text = { Text("Tu cuenta ha sido creada con éxito. Serás redirigido al inicio de sesión para continuar.") },
+                    confirmButton = {
+                        Button(
+                            onClick = {
+                                showSuccessDialog = false
+
+                                if (finalResult == RegistrationResult.SUCCESS) {
+                                    navController.navigate("login_screen") {
+                                        popUpTo(navController.graph.id) { inclusive = true }
+                                    }
+                                }
+                                viewModel.resetUiState()
+                            },
+                        ) {
+                            Text("Ir a Iniciar Sesión")
+                        }
+                    }
+                )
+            }
+        }
+    }
 
 }
